a6f2e69658b477ec9d0031626a9d621d05122aa3
anggaferdani <eanggawahyu@gmail.com>
2025-09-15 21:01:37 +0700

    format

--- app/Imports/ShiftImport.php
+++ app/Imports/ShiftImport.php
@@ -18,7 +18,7 @@ class ShiftImport implements ToCollection
         foreach ($rows as $row) {
             if (empty($row[0])) continue;
 
-            $tanggal = \Carbon\Carbon::parse($row[0])->format('Y-m-d');
+        $tanggal = \Carbon\Carbon::createFromFormat('Y-m-d', $row[0])->format('Y-m-d');
 
             foreach ($row as $colIndex => $value) {
                 if ($colIndex === 0 || empty($value)) continue;
